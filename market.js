27804073de49e051f3d72a2a786eef359742b2a7
[mohmmedlahlali20] <[mohmmedlaeh81@gmail.com]>
2023-10-25 18:49:13 +0100

    new-commit

--- market.js
+++ market.js
@@ -1,14 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var myModal = document.getElementById('myModal');2
-    if (myModal) {
-        myModal.classList.add('show');
-    }
-});
-document.addEventListener('DOMContentLoaded', function () {
+
     var usernameInput = document.getElementById('UserName');
     var passwordInput = document.getElementById('Password');
-    var alertMessage = document.querySelector('.alert');
     var loginButton = document.querySelector('.log-btn');
+    console.log(loginButton);
 
     loginButton.addEventListener('click', function () {
         var username = usernameInput.value;
@@ -20,18 +14,18 @@ document.addEventListener('DOMContentLoaded', function () {
         var isVlidPassword = validatePassword(password);
 
         if (username === isValidEmail && password === isVlidPassword) {
-            console.log('Success!');
+            window.location.href = 'Acceuil.html';
+            console.log('Valid');
         } else {
             // get elemet by id or class to show invalid message
             console.log('Invalid');
         }
     });
-});
 
 function validateEmail(email) {
 
     // Regular expression for a basic email validation
-    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; 
     return emailRegex.test(email);
 
 }
@@ -41,3 +35,10 @@ function validatePassword(password) {
     var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return passwordRegex.test(password);
 }
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+document.toUpperCase();
